42ef09c696ed7bb8bc53212e92532a74231a1829
Brady Silvers <toastedflqme@gmail.com>
2025-08-28 20:19:38 -0700

    feat: added complete extension popup menu
    - search for professor on ratemyprofessor
    - button to open GOLD in new tab
    - link to github project

--- source/popup/search.js
+++ source/popup/search.js
@@ -3,12 +3,27 @@ function handleSearch(inputProfName) {
     window.open(url, '_blank');
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
+    const versionElement = document.getElementById('version');
     const form = document.querySelector('form');
     const input = document.getElementById('search');
+    const searchButton = document.getElementById('search-button');
+    const openGoldButton = document.getElementById('open-gold-button');
+
+    versionElement.textContent = chrome.runtime.getManifest().version;
+
     input.focus();
-    form.onsubmit = function(e) {
-        e.preventDefault();
-        handleSearch(input.value);
+    input.addEventListener('input', () => {
+        searchButton.disabled = input.value.trim().length === 0;
+    });
+
+    form.onsubmit = (event) => {
+        event.preventDefault();
+        handleSearch(input.value.toUpperCase());
     };
+
+    openGoldButton.addEventListener('click', () => {
+        window.open('https://my.sa.ucsb.edu/gold/', '_blank');
+    });
+
 });
