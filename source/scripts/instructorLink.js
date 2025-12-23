c216664b6d42d144cb81230c240ec8d063536dc6
Chazz Yu <chazzyu6@gmail.com>
2025-12-23 12:57:40 -0800

    fix: RMP links apply to multiple instructors of single class
    - revert back to negative selection for professorLink
    - rename professorLink.js
    - move functions in utils.js to instructorLink.js

---
+++ source/scripts/instructorLink.js
@@ -0,0 +1,48 @@
+// from utils.js
+/* global SEARCH_URL */
+
+(() => {
+  /**
+   * div
+   * └─ label.visible-xs ("Instructor") <- instructorLabels[i]
+   * └─ "MIRZA D" <- target
+   */
+  const instructorLabels = Array.from(document.querySelectorAll('label.visible-xs'))
+    .filter(label => label.textContent === 'Instructor');
+
+  instructorLabels.forEach(label => {
+    label.parentElement.childNodes.forEach(
+      node => {
+        if (!(node.nodeType === Node.ELEMENT_NODE && label.matches('label.visible-xs')))
+          convertNodeToRmpLink(node);
+      });
+  });
+})();
+
+
+function profNameIsValid(profName) {
+  const name = profName.toLowerCase().trim();
+  const INVALID_PROF_NAMES = [ 'cancel', 't.b.a', 't.b.a.', '', null ];
+  return !INVALID_PROF_NAMES.includes(name);
+}
+
+function createRmpLink(profName) {
+  const ratemyprofURL = SEARCH_URL + encodeURIComponent(profName);
+
+  const link = document.createElement('a');
+  link.href = ratemyprofURL;
+  link.rel = 'noopener noreferrer';
+  link.textContent = `${profName}`;
+  link.target = '_blank';
+  link.style = "white-space: nowrap;";
+
+  return link;
+}
+
+function convertNodeToRmpLink(node) {
+  const profName = node?.textContent?.trim();
+  if (profName && profNameIsValid(profName)) {
+    const link = createRmpLink(profName);
+    node.replaceWith(link);
+  }
+}
--- source/scripts/utils.js
+++ source/scripts/utils.js
@@ -1,30 +1,3 @@
-/* exported convertNodeToRmpLink */
+/* exported SEARCH_URL */
 
 const SEARCH_URL = 'https://www.ratemyprofessors.com/search/professors/1077?q='
-
-function profNameIsValid(profName) {
-  const name = profName.toLowerCase().trim();
-  const INVALID_PROF_NAMES = [ 'cancel', 't.b.a', 't.b.a.', '', null ];
-  return !INVALID_PROF_NAMES.includes(name);
-}
-
-function createRmpLink(profName) {
-  const ratemyprofURL = SEARCH_URL + encodeURIComponent(profName);
-
-  const link = document.createElement('a');
-  link.href = ratemyprofURL;
-  link.rel = 'noopener noreferrer';
-  link.textContent = `${profName}`;
-  link.target = '_blank';
-  link.style = "white-space: nowrap;";
-
-  return link;
-}
-
-function convertNodeToRmpLink(node) {
-  const profName = node?.textContent?.trim();
-  if (profName && profNameIsValid(profName)) {
-    const link = createRmpLink(profName);
-    node.replaceWith(link);
-  }
-}
