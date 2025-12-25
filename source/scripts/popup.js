0388e46e3189aedcb0af3b65a641eccf60eebcd2
Chazz Yu <chazzyu6@gmail.com>
2025-12-24 18:01:48 -0800

    refractor: rename files to kebab-case

--- source/scripts/popup.js
+++ source/scripts/popup.js
@@ -1,31 +1,31 @@
 /* global chrome, SEARCH_URL */
 
 function handleSearch(inputProfName) {
-    const url = SEARCH_URL + encodeURIComponent(inputProfName.trim());
-    window.open(url, '_blank');
+  const url = SEARCH_URL + encodeURIComponent(inputProfName.trim());
+  window.open(url, '_blank');
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const versionElement = document.getElementById('version');
-    const form = document.querySelector('form');
-    const input = document.getElementById('search');
-    const searchButton = document.getElementById('search-button');
-    const openGoldButton = document.getElementById('open-gold-button');
+  const versionElement = document.getElementById('version');
+  const form = document.querySelector('form');
+  const input = document.getElementById('search');
+  const searchButton = document.getElementById('search-button');
+  const openGoldButton = document.getElementById('open-gold-button');
 
-    versionElement.textContent = chrome.runtime.getManifest().version;
+  versionElement.textContent = chrome.runtime.getManifest().version;
 
-    input.focus();
-    input.addEventListener('input', () => {
-        searchButton.disabled = input.value.trim().length === 0;
-    });
+  input.focus();
+  input.addEventListener('input', () => {
+    searchButton.disabled = input.value.trim().length === 0;
+  });
 
-    form.onsubmit = (event) => {
-        event.preventDefault();
-        handleSearch(input.value.toUpperCase());
-    };
+  form.onsubmit = (event) => {
+    event.preventDefault();
+    handleSearch(input.value.toUpperCase());
+  };
 
-    openGoldButton.addEventListener('click', () => {
-        window.open('https://my.sa.ucsb.edu/gold/', '_blank');
-    });
+  openGoldButton.addEventListener('click', () => {
+    window.open('https://my.sa.ucsb.edu/gold/', '_blank');
+  });
 
 });
